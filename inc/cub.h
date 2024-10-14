dbb09012b0e60d01a28fa4e66c3b9cde25712022
Edgar Miro Cortes <emiro-co@car7s6.42barcelona.com>
2024-10-14 22:02:34 +0200

    planteamiento parsing

--- inc/cub.h
+++ inc/cub.h
@@ -2,7 +2,11 @@
 # define CUB_H
 
 // 	---Includes---
-
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/stat.h>
+# include <fcntl.h> 
 
 //	---Structures---
 typedef struct s_vector
@@ -22,18 +26,51 @@ typedef struct s_color
 
 typedef struct s_map
 {
+	t_check		check;
 	char		**content;
 	char 		*north;
 	char 		*south;
 	char 		*east;
 	char 		*west;
 	t_color 	floor;
-	t_color 	sky;
+	t_color 	celling;
 }	t_map;
+
+typedef struct s_check
+{
+	// inicializar todos los errores posibles
+	int			format_error;
+	int			path_error;
+	int			invalid_character_error;
+	int			map_design_error;
+	int			no_init_pos_error;
+	int			duplicate_init_pos_error;
+	int			no_walls_error;
+
+	// inicializar todos los datos necesarios
+	// para el check pero no para el programa
+	char		*path;
+	char		**all_doc;
+	char		**check_map;
+	int			init_pos_found;
+	int			lines_to_map;
+	int			map_lines;
+	int			map_columns;
+}	t_check;
 
 //	--- MAIN.C ---
 
 //	--- PARSE.C ---
+void	ft_check_input(t_map *map, int argc, char *argv[]);
+void	ft_get_doc(t_map *map, char *path, int size);
+int		ft_get_rows(t_map *map, char *path);
+t_map	ft_parse(char *path);
+char	*clean_line(char *line);
 
 //	--- GAME.C ---
+
+//	--- ERROR_FREE.C ---
+void free_all_doc(char ***all_doc);
+void ft_error(const char *msg);
+
 #endif
