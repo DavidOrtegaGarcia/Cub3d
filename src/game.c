d259dd440c62c1ca3de06359f8b1ae308d5182cb
DavidOrtegaGarcia <36156063+DavidOrtegaGarcia@users.noreply.github.com>
2024-11-18 17:18:50 +0100

    Update game.c

--- src/game.c
+++ src/game.c
@@ -2,8 +2,6 @@
 
 #include "cub.h"
 
-//#define BPP sizeof(int32_t) //Bytes per pixel
-
 void resize_hook(int32_t width, int32_t height, void *param)
 {
 	mlx_image_t *img = (mlx_image_t*)param;
@@ -13,13 +11,28 @@ void resize_hook(int32_t width, int32_t height, void *param)
 void exec_game(void *param)
 {
 	t_mlx *tmlx;
+	t_dpoint mov;
 
 	tmlx = param; 
 	mlx_delete_image(tmlx->mlx, tmlx->img);
 	tmlx->img = mlx_new_image(tmlx->mlx, S_WIDTH, S_HEIGHT); // create new image
-	//hook(mlx, 0, 0); // hook the player
+	movment_hook(tmlx, mov); // hook the player
 	cast_rays(tmlx); // cast the rays
 	mlx_image_to_window(tmlx->mlx, tmlx->img, 0, 0);
+}
+
+double get_viwew_dir(t_mlx tmlx)
+{
+
+	if (tmlx.tmap.orientation == 'N')
+		return (3 * M_PI / 2);
+	if (tmlx.tmap.orientation == 'S')
+		return (M_PI / 2);
+	if (tmlx.tmap.orientation == 'W')
+		return (M_PI);
+	if (tmlx.tmap.orientation == 'E')
+		return (0);
+
 }
 
 void init_tplayer(t_mlx tmlx)
@@ -27,7 +40,7 @@ void init_tplayer(t_mlx tmlx)
 	tmlx.tplyr->pos_px.x = tmlx.tmap.init_point.x * BOX_SIZE + BOX_SIZE / 2; //BOX_SIZE / 2 CENTRA EL JUGADOR EN MEDIO DE LA CASILLA 
 	tmlx.tplyr->pos_px.y = tmlx.tmap.init_point.y * BOX_SIZE + BOX_SIZE / 2;
 	tmlx.tplyr->fov_rad = (FOV * M_PI) / 180; //Convert fov to radius
-	tmlx.tplyr->view_dir = M_PI / 2; // Orientaton of the player
+	tmlx.tplyr->view_dir = get_viwew_dir(tmlx); // Orientaton of the player
 }
 
 void init_game(t_map tmap)
@@ -41,7 +54,8 @@ void init_game(t_map tmap)
 	if (!tmlx.mlx)
 		exit(EXIT_FAILURE);
 	init_tplayer(tmlx);
-	//tmlx.img = mlx_new_image(tmlx.mlx, S_WIDTH, S_HEIGHT);
+	tmlx.img = mlx_new_image(&tmlx.mlx, S_WIDTH, S_HEIGHT);
+	mlx_key_hook(tmlx.mlx, &key_hook, &tmlx);
 	mlx_loop_hook(tmlx.mlx, &exec_game, &tmlx);
 	mlx_resize_hook(tmlx.mlx, &resize_hook, tmlx.img);
 	mlx_loop(tmlx.mlx);
